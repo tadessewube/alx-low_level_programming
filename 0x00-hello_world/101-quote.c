75a4e6f8d14da04a21b65e96a58191da2e75a0bf
tadessewube <softwareteam68@gmail.com>
2022-04-07 06:32:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,8 @@
  */
 int main(void)
 {
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61)
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-
-
-			return (1);
+	return (1);
 
 }
