241b1d8c5dd5eee0c6f20ea0da3bceace74ba947
tadessewube <softwareteam68@gmail.com>
2022-04-07 05:31:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-
-
-/**
+/** 
+ * main - Entry point
+ 
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ 
+ * Return: Always 0 (Success)
+ 
+ */
 
 int main(void)
 
