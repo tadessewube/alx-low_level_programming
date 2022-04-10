81eb8f4c29ad7d788c6eac6d15ed3e9b0ba69324
tadessewube <softwareteam68@gmail.com>
2022-04-10 09:27:01 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,19 +8,19 @@
 int main(void)
 {
 	char c;
-
+	
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
-
+	
 	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
-
+	
 	putchar('\n');
-
+	
 	return (0);
 }
 	
