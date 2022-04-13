b1f2c2fcbdc721b90caddacf160dd657f6e728a3
softwareteam68@gmail.com <softwareteam68@gmail.com>
2022-04-13 21:28:05 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- * main - prints the string "_putchar" from a character array.
- * Return: 0 on success.
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char c[] = "_putchar";
-        int i;
-
-        for (i = 0; i < 9; i++)
-        {
-                _putchar(c[i]);
-        }
-        _putchar('\n');
-
-        return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
