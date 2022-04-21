91a6ac22fc511a1bfe145563c9e82129d0295c84
softwareteam68@gmail.com <softwareteam68@gmail.com>
2022-04-21 21:27:03 +0300

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
