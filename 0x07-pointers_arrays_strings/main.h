3f0326b97840972b1d5351f06454fac5b002b206
softwareteam68@gmail.com <softwareteam68@gmail.com>
2022-04-25 20:45:38 +0300

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,18 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
-
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-char *_memset(char *s, char b, unsigned int n)
+#ifndef MAIN_H
+#define MAIN_H
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -21,5 +9,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
+int _putchar(char c);
+#endif /*MAIN_H*/
