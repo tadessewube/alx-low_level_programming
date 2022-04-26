74cf69fcbf07ab7cd554d6c9709d95498ff37fe6
softwareteam68@gmail.com <softwareteam68@gmail.com>
2022-04-26 14:57:21 +0300

    main

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,26 @@
+/*
+ * File: 0-memset.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
 
 /**
-  * _memset - Fill memory with a constant byte
-  * @s: memory area to fill
-  * @b: constant byte to fill
-  * @n: bytes of memory area to fill
-  *
-  * Return: the memory area filled
-  */
-char *_memset(char *s, char b, unsigned int n)
+ * memset - Fills the first n bytes of the memory area
+ *          pointed to by @s with the constant byte @c.
+ * @s: A pointer to the memory area to be filled.
+ * @c: The character to fill the memory area with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the filled memory area @s.
+ */
+void *_memset(void *s, int c, size_t n)
 {
-	unsigned int a;
+	unsigned int index;
+	unsigned char *memory = s, value = c;
 
-	for (a = 0; a < n; a++)
-	{
-		s[a] = b;
-	}
+	for (index = 0; index < n; index++)
+		memory[index] = value;
 
-	return (s);
+	return (memory);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void *_memset(void *s, int c, size_t n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
