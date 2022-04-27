93a62e07060b9895df6f85bc100dcc4e7831713e
softwareteam68@gmail.com <softwareteam68@gmail.com>
2022-04-27 19:53:28 +0300

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: number
+ * @y: number of power
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
+© 2022 GitHub, Inc.
