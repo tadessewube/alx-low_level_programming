99f47da81bc35c3e34bc1bf1a83181f6afb38e03
tadessewube <softwareteam68@gmail.com>
2022-05-02 17:02:14 +0300

    main

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,14 +3,20 @@
 
 int _putchar(char c);
 
-void *malloc_checked(unsigned int b);
+char *create_array(unsigned int size, char c);
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *_strdup(char *str);
 
-void *_calloc(unsigned int nmemb, unsigned int size);
+int _strlen(char *s);
 
-int *array_range(int min, int max);
+char *str_concat(char *s1, char *s2);
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_strncat(char *dest, char *src, int n);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
 
 #endif
