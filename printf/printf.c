d3f0000e59736c51072375ce80d49d52696b90e9
tadessewube <softwareteam68@gmail.com>
2022-05-16 13:32:29 +0300

    Update Printf

---
+++ printf/printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _printf - prints any string with certain flags for modification
+ * @format: the string of characters to write to buffer
+ * Return: an integer that counts how many writes to the buffer were made
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, var = 0;
+	va_list v_ls;
+	buffer *buf;
+
+	buf = buf_new();
+	if (buf == NULL)
+		return (-1);
+	if (format == NULL)
+		return (-1);
+	va_start(v_ls, format);
+	while (format[i])
+	{
+		buf_wr(buf);
+		if (format[i] == '%')
+		{
+			var = opid(buf, v_ls, format, i);
+			if (var < 0)
+			{
+				i = var;
+				break;
+			}
+			i += var;
+			continue;
+		}
+		buf->str[buf->index] = format[i];
+		buf_inc(buf);
+		i++;
+	}
+	buf_write(buf);
+	if (var >= 0)
+		i = buf->overflow;
+	buf_end(buf);
+	va_end(v_ls);
+	return (i);
+}
